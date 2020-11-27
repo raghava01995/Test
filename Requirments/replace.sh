293540ec4507c331461917e27dc816cb2aac82c8
raghava01995 <raghurockz99@gmail.com>
2020-11-27 10:17:54 +0000

    6th test with full path for docker-compose file

--- Requirments/replace.sh
+++ Requirments/replace.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sed -i "s/con_name/$1/g" /home/raghu/jenkins-data/jen_docker_job/Test/Requirments/docker-compose.yml 
-docker-compose up -d
-docker cp ../$2 $1:/usr/share/nginx/html/index.html
+docker-compose -f /home/raghu/jenkins-data/jen_docker_job/Test/Requirments/docker-compose.yml up -d
+docker cp /home/raghu/jenkins-data/jen_docker_job/Test/$2 $1:/usr/share/nginx/html/index.html
 
